1ff70e1e1c22e963b3c2fd783a5fec459ec00a8e
Larry Sprock <larry@lucidbleu.com>
2018-11-06 11:03:22 -0800

    Rename template

--- addon/components/slick-slider.js
+++ addon/components/slick-slider.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import layout from '../templates/components/ember-cli-slick';
+import layout from '../templates/components/slick-slider';
 import { scheduleOnce } from '@ember/runloop';
 
 const responsive = [];
